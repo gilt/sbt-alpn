0de0e8bd0e2431206a5378867dd03153386be90f
Graham Rhodes <graham.a.r@gmail.com>
2016-05-12 17:34:41 +0900

    Initial commit

---
+++ src/main/scala/JettyAlpn.scala
@@ -0,0 +1,49 @@
+package com.gilt.sbt.alpn
+
+import sbt._
+import sbt.Keys._
+
+import com.typesafe.sbt.SbtNativePackager._
+import com.typesafe.sbt.packager.archetypes.JavaAppPackaging
+import com.typesafe.sbt.packager.archetypes.JavaAppPackaging.autoImport.bashScriptExtraDefines
+
+object JettyAlpn extends AutoPlugin {
+  object autoImport {
+    val alpnApiIncluded = settingKey[Boolean]("Include ALPN api as a dependency")
+    val alpnApiVersion = settingKey[String]("ALPN api version")
+    val alpnAgentVersion = settingKey[String]("ALPN boot version")
+    val alpnAgent = taskKey[File]("ALPN agent jar location")
+  }
+
+  import autoImport._
+
+  override def requires = JavaAppPackaging
+
+  val alpnConfig = config("alpn-agent").hide
+
+  override lazy val projectSettings = Seq(
+    ivyConfigurations += alpnConfig,
+    alpnApiIncluded := false,
+    alpnApiVersion := "1.1.2.v20150522",
+    alpnAgentVersion := "2.0.2",
+    alpnAgent := findAlpnAgent(update.value),
+    libraryDependencies ++= {
+      if(alpnApiIncluded.value) {
+        Seq(
+          "org.eclipse.jetty.alpn" % "alpn-api" % alpnApiVersion.value,
+          "org.mortbay.jetty.alpn" % "jetty-alpn-agent" % alpnAgentVersion.value % alpnConfig
+        )
+      } else {
+        Seq("org.mortbay.jetty.alpn" % "jetty-alpn-agent" % alpnAgentVersion.value % alpnConfig)
+      }
+    },
+    mappings in Universal += alpnAgent.value -> "alpn/jetty-alpn-agent.jar",
+    bashScriptExtraDefines += """addJava "-javaagent:${app_home}/../alpn/jetty-alpn-agent.jar""""
+  )
+
+  private[this] val alpnAgentFilter: DependencyFilter =
+    configurationFilter("alpn-agent") && artifactFilter(`type` = "jar")
+
+  def findAlpnAgent(report: UpdateReport) = report.matching(alpnAgentFilter).head
+}
+
